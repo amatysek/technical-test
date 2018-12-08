322f8c7a9af537ab5eda41ed17d9f9d9140c6b6b
Aila Matysek <ailamariematysek@gmail.com>
2018-12-08 12:12:58 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#add-text").text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab fugiat dolores corporis recusandae temporibus minus porro quas. Deserunt necessitatibus officiis, sunt voluptas itaque numquam. Recusandae!");
+});
+
+$('.confirmation').on('click', function () {
+    return confirm('Would you like to read our legal policy?');
+});
